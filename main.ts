d55150e4a0f68e4d4e6b5b2088afd03e66439ede
Sigi71 <Sigi71@users.noreply.github.com>
2022-01-23 13:33:28 +0100

    Initial files for MakeCode project

---
+++ main.ts
@@ -0,0 +1,90 @@
+function StartNewPlane () {
+    plane = sprites.create(img`
+        ..ccc.........ffffff....
+        ..f4cc.......fcc22ff....
+        ..f44cc...fffccccff.....
+        ..f244cccc22224442cc....
+        ..f224cc2222222244b9c...
+        ..cf2222222222222b999c..
+        .c22c222222222b11199b2c.
+        f22ccccccc222299111b222c
+        fffffcc222c222222222222f
+        .....f2222442222222222f.
+        ....f222244fc2222222ff..
+        ...c222244ffffffffff....
+        ...c2222cfffc2f.........
+        ...ffffffff2ccf.........
+        .......ffff2cf..........
+        ........fffff...........
+        `, SpriteKind.Player)
+    plane.x += -50
+    controller.moveSprite(plane, 200, 200)
+    plane.startEffect(effects.bubbles, 100)
+    plane.setStayInScreen(true)
+}
+controller.A.onEvent(ControllerButtonEvent.Pressed, function () {
+    projectile = sprites.createProjectileFromSprite(img`
+        . . . . . . . . . . . . . . . . 
+        . . . . . . . . . . . . . . . . 
+        . . . . . . . . . . . . . . . . 
+        . . . . . . . . . . . . . . . . 
+        . . . . . . . . . . . . . . . . 
+        . . . . . . . . . . . . . . . . 
+        . . . . . . . 4 4 . . . . . . . 
+        . . . . . . 4 5 5 4 . . . . . . 
+        . . . . . . 2 5 5 2 . . . . . . 
+        . . . . . . . 2 2 . . . . . . . 
+        . . . . . . . . . . . . . . . . 
+        . . . . . . . . . . . . . . . . 
+        . . . . . . . . . . . . . . . . 
+        . . . . . . . . . . . . . . . . 
+        . . . . . . . . . . . . . . . . 
+        . . . . . . . . . . . . . . . . 
+        `, plane, 50, 0)
+    projectile.setFlag(SpriteFlag.AutoDestroy, true)
+})
+sprites.onOverlap(SpriteKind.Enemy, SpriteKind.Player, function (sprite, otherSprite) {
+    plane.destroy()
+    scene.cameraShake(3, 500)
+    info.changeLifeBy(-1)
+    for (let value of sprites.allOfKind(SpriteKind.Enemy)) {
+        value.destroy(effects.bubbles, 10)
+    }
+    StartNewPlane()
+})
+info.onLifeZero(function () {
+    game.over(false, effects.splatter)
+})
+sprites.onOverlap(SpriteKind.Projectile, SpriteKind.Enemy, function (sprite, otherSprite) {
+    otherSprite.destroy(effects.fire, 500)
+    info.changeScoreBy(1)
+    projectile.destroy()
+})
+let spaceenemy: Sprite = null
+let projectile: Sprite = null
+let plane: Sprite = null
+StartNewPlane()
+info.setLife(3)
+game.onUpdateInterval(1000, function () {
+    spaceenemy = sprites.create(img`
+        . . . . . b b b b b b . . . . . 
+        . . . b b 9 9 9 9 9 9 b b . . . 
+        . . b b 9 9 9 9 9 9 9 9 b b . . 
+        . b b 9 d 9 9 9 9 9 9 9 9 b b . 
+        . b 9 d 9 9 9 9 9 1 1 1 9 9 b . 
+        b 9 d d 9 9 9 9 9 1 1 1 9 9 9 b 
+        b 9 d 9 9 9 9 9 9 1 1 1 9 9 9 b 
+        b 9 3 9 9 9 9 9 9 9 9 9 1 9 9 b 
+        b 5 3 d 9 9 9 9 9 9 9 9 9 9 9 b 
+        b 5 3 3 9 9 9 9 9 9 9 9 9 d 9 b 
+        b 5 d 3 3 9 9 9 9 9 9 9 d d 9 b 
+        . b 5 3 3 3 d 9 9 9 9 d d 5 b . 
+        . b d 5 3 3 3 3 3 3 3 d 5 b b . 
+        . . b d 5 d 3 3 3 3 5 5 b b . . 
+        . . . b b 5 5 5 5 5 5 b b . . . 
+        . . . . . b b b b b b . . . . . 
+        `, SpriteKind.Enemy)
+    spaceenemy.setVelocity(-50, 0)
+    spaceenemy.setPosition(160, randint(5, 115))
+    spaceenemy.setFlag(SpriteFlag.AutoDestroy, true)
+})
