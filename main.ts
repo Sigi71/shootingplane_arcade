13a579017d8deb16142ad4f4687a679b72ceaf61
Sigi71 <Sigi71@users.noreply.github.com>
2022-01-26 19:40:18 +0100

    Update pxt.json, main.blocks, main.ts, tilemap.g.jres, tilemap.g.ts

--- main.ts
+++ main.ts
@@ -4,6 +4,8 @@ namespace SpriteKind {
 namespace StatusBarKind {
     export const Ammo = StatusBarKind.create()
 }
+// vypusteni noveho letadla do hry
+// 
 function StartNewPlane (speed: number) {
     plane = sprites.create(img`
         ..fff.........ffffff....
@@ -33,6 +35,16 @@ function StartNewPlane (speed: number) {
     plane.startEffect(effects.trail, 100)
     plane.setStayInScreen(true)
 }
+controller.B.onEvent(ControllerButtonEvent.Pressed, function () {
+    if (act_level == 1) {
+        act_level = 2
+        game.splash("LEVEL 2")
+    } else {
+        act_level = 1
+        game.splash("LEVEL 1")
+    }
+})
+// Vystreleni projektilu
 controller.A.onEvent(ControllerButtonEvent.Pressed, function () {
     if (statusbar_ammo.value > 0) {
         projectile = sprites.createProjectileFromSprite(img`
@@ -57,6 +69,30 @@ controller.A.onEvent(ControllerButtonEvent.Pressed, function () {
         statusbar_ammo.value += -1
     }
 })
+function Level2_spawnEnemy () {
+    spaceenemy = sprites.create(img`
+        . . . . . b b b b b b . . . . . 
+        . . . b b 9 9 9 9 9 9 b b . . . 
+        . . b b 9 9 9 9 9 9 9 9 b b . . 
+        . b b 9 d 9 9 9 9 9 9 9 9 b b . 
+        . b 9 d 9 9 9 9 9 1 1 1 9 9 b . 
+        b 9 d d 9 9 9 9 9 1 1 1 9 9 9 b 
+        b 9 d 9 9 9 9 9 9 1 1 1 9 9 9 b 
+        b 9 3 9 9 9 9 9 9 9 9 9 1 9 9 b 
+        b 5 3 d 9 9 9 9 9 9 9 9 9 9 9 b 
+        b 5 3 3 9 9 9 9 9 9 9 9 9 d 9 b 
+        b 5 d 3 3 9 9 9 9 9 9 9 d d 9 b 
+        . b 5 3 3 3 d 9 9 9 9 d d 5 b . 
+        . b d 5 3 3 3 3 3 3 3 d 5 b b . 
+        . . b d 5 d 3 3 3 3 5 5 b b . . 
+        . . . b b 5 5 5 5 5 5 b b . . . 
+        . . . . . b b b b b b . . . . . 
+        `, SpriteKind.Enemy)
+    spaceenemy.setVelocity(randint(-55, -40), randint(-25, 25))
+    spaceenemy.setPosition(160, randint(5, 115))
+    spaceenemy.setFlag(SpriteFlag.AutoDestroy, true)
+    spaceenemy.setBounceOnWall(true)
+}
 // Zasah letadla bublinou
 sprites.onOverlap(SpriteKind.Enemy, SpriteKind.Player, function (sprite, otherSprite) {
     plane.destroy()
@@ -67,9 +103,33 @@ sprites.onOverlap(SpriteKind.Enemy, SpriteKind.Player, function (sprite, otherSp
     }
     StartNewPlane(planespeed)
 })
+function Level1_spawnEnemy () {
+    spaceenemy = sprites.create(img`
+        . . . . . . . . c c c c . . . . 
+        . . . . c c c c c c c c c . . . 
+        . . . c f c c a a a a c a c . . 
+        . . c c f f f f a a a c a a c . 
+        . . c c a f f c a a f f f a a c 
+        . . c c a a a a b c f f f a a c 
+        . c c c c a c c b a f c a a c c 
+        c a f f c c c a b b 6 b b b c c 
+        c a f f f f c c c 6 b b b a a c 
+        c a a c f f c a 6 6 b b b a a c 
+        c c b a a a a b 6 b b a b b a . 
+        . c c b b b b b b b a c c b a . 
+        . . c c c b c c c b a a b c . . 
+        . . . . c b a c c b b b c . . . 
+        . . . . c b b a a 6 b c . . . . 
+        . . . . . . b 6 6 c c . . . . . 
+        `, SpriteKind.Enemy)
+    spaceenemy.setVelocity(randint(-55, -40), 0)
+    spaceenemy.setPosition(160, randint(5, 115))
+    spaceenemy.setFlag(SpriteFlag.AutoDestroy, true)
+}
 info.onLifeZero(function () {
     game.over(false, effects.melt)
 })
+// letadlo zachyti Ammo pack
 sprites.onOverlap(SpriteKind.Ammo, SpriteKind.Player, function (sprite, otherSprite) {
     sprite.setImage(img`
         . . . . . . . . . . . . . . . . 
@@ -92,7 +152,7 @@ sprites.onOverlap(SpriteKind.Ammo, SpriteKind.Player, function (sprite, otherSpr
     sprite.destroy(effects.starField, 1000)
     statusbar_ammo.value += 10
 })
-// Sestreleni bubliny
+// Sestreleni Amoo packu
 sprites.onOverlap(SpriteKind.Projectile, SpriteKind.Ammo, function (sprite, otherSprite) {
     otherSprite.setImage(img`
         . 3 . . . . . . . . . . . 4 . . 
@@ -144,32 +204,18 @@ let spaceenemy: Sprite = null
 let projectile: Sprite = null
 let statusbar_ammo: StatusBarSprite = null
 let plane: Sprite = null
+let act_level = 0
 let planespeed = 0
 planespeed = 50
+act_level = 1
 StartNewPlane(planespeed)
 info.setLife(3)
 game.onUpdateInterval(1000, function () {
-    spaceenemy = sprites.create(img`
-        . . . . . . . . c c c c . . . . 
-        . . . . c c c c c c c c c . . . 
-        . . . c f c c a a a a c a c . . 
-        . . c c f f f f a a a c a a c . 
-        . . c c a f f c a a f f f a a c 
-        . . c c a a a a b c f f f a a c 
-        . c c c c a c c b a f c a a c c 
-        c a f f c c c a b b 6 b b b c c 
-        c a f f f f c c c 6 b b b a a c 
-        c a a c f f c a 6 6 b b b a a c 
-        c c b a a a a b 6 b b a b b a . 
-        . c c b b b b b b b a c c b a . 
-        . . c c c b c c c b a a b c . . 
-        . . . . c b a c c b b b c . . . 
-        . . . . c b b a a 6 b c . . . . 
-        . . . . . . b 6 6 c c . . . . . 
-        `, SpriteKind.Enemy)
-    spaceenemy.setVelocity(randint(-55, -40), 0)
-    spaceenemy.setPosition(160, randint(5, 115))
-    spaceenemy.setFlag(SpriteFlag.AutoDestroy, true)
+    if (act_level == 1) {
+        Level1_spawnEnemy()
+    } else {
+        Level2_spawnEnemy()
+    }
     if (statusbar_ammo.value <= 40) {
         if (Math.percentChance(15)) {
             ammo = sprites.create(img`
