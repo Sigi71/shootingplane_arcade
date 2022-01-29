134c00988c794eecfb56eb32e4958d9b58a50581
Adamoso5442 <98040983+Adamoso5442@users.noreply.github.com>
2022-01-29 19:29:45 +0100

    Update pxt.json, main.blocks, main.ts

--- main.ts
+++ main.ts
@@ -5,7 +5,6 @@ namespace StatusBarKind {
     export const Ammo = StatusBarKind.create()
 }
 // vypusteni noveho letadla do hry
-// 
 function StartNewPlane (speed: number) {
     plane = sprites.create(img`
         ..fff.........ffffff....
@@ -71,22 +70,22 @@ controller.A.onEvent(ControllerButtonEvent.Pressed, function () {
 })
 function Level2_spawnEnemy () {
     spaceenemy = sprites.create(img`
-        . . . . . b b b b b b . . . . . 
-        . . . b b 9 9 9 9 9 9 b b . . . 
-        . . b b 9 9 9 9 9 9 9 9 b b . . 
-        . b b 9 d 9 9 9 9 9 9 9 9 b b . 
-        . b 9 d 9 9 9 9 9 1 1 1 9 9 b . 
-        b 9 d d 9 9 9 9 9 1 1 1 9 9 9 b 
-        b 9 d 9 9 9 9 9 9 1 1 1 9 9 9 b 
-        b 9 3 9 9 9 9 9 9 9 9 9 1 9 9 b 
-        b 5 3 d 9 9 9 9 9 9 9 9 9 9 9 b 
-        b 5 3 3 9 9 9 9 9 9 9 9 9 d 9 b 
-        b 5 d 3 3 9 9 9 9 9 9 9 d d 9 b 
-        . b 5 3 3 3 d 9 9 9 9 d d 5 b . 
-        . b d 5 3 3 3 3 3 3 3 d 5 b b . 
-        . . b d 5 d 3 3 3 3 5 5 b b . . 
-        . . . b b 5 5 5 5 5 5 b b . . . 
-        . . . . . b b b b b b . . . . . 
+        . . . . . . . . c c c c . . . . 
+        . . . . c c c c c c c c c . . . 
+        . . . c f c c a a a a c a c . . 
+        . . c c f f f f a a a c a a c . 
+        . . c c a f f c a a f f f a a c 
+        . . c c a a a a b c f f f a a c 
+        . c c c c a c c b a f c a a c c 
+        c a f f c c c a b b 6 b b b c c 
+        c a f f f f c c c 6 b b b a a c 
+        c a a c f f c a 6 6 b b b a a c 
+        c c b a a a a b 6 b b a b b a . 
+        . c c b b b b b b b a c c b a . 
+        . . c c c b c c c b a a b c . . 
+        . . . . c b a c c b b b c . . . 
+        . . . . c b b a a 6 b c . . . . 
+        . . . . . . b 6 6 c c . . . . . 
         `, SpriteKind.Enemy)
     spaceenemy.setVelocity(randint(-55, -40), randint(-25, 25))
     spaceenemy.setPosition(160, randint(5, 115))
@@ -206,7 +205,7 @@ let statusbar_ammo: StatusBarSprite = null
 let plane: Sprite = null
 let act_level = 0
 let planespeed = 0
-planespeed = 50
+planespeed = 75
 act_level = 1
 StartNewPlane(planespeed)
 info.setLife(3)
@@ -230,9 +229,9 @@ game.onUpdateInterval(1000, function () {
                 ......66666666......
                 ......77777777......
                 .....7797777777.....
-                .....751f5f1177.....
-                .....2415f51127.....
-                .....751f5f1177.....
+                .....7977777777.....
+                .....7977777777.....
+                .....7777777777.....
                 .....7777777777.....
                 .....7777777776.....
                 .....7777777776.....
